2338997c8f8387b602a859b2fb439e4ca6d72688
KichangChoi <90402191+Choikichang@users.noreply.github.com>
2025-04-24 22:05:14 -0400

    Update optical_flow.py

--- optical_flow.py
+++ optical_flow.py
@@ -1,8 +1,7 @@
-# predict_pouring.py
-
 import cv2
 import numpy as np
 import logging
+import time  # time 모듈 추가
 
 # Set up logger
 log = logging.getLogger('detect_pouring')
@@ -20,21 +19,32 @@ class PouringDetector:
         self.motion_detected = False
         self.video_writer = None
         self.video_segment = []
-
+        
+        # 기준 프레임 리셋을 위한 변수 추가
+        self.last_reset_time = time.time()
+        self.reset_interval = 30 * 60  # 30분(초 단위)
+        
     def detect_pouring(self, frame):
         try:
             next_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+            
+            # 30분마다 prev_frame 리셋
+            current_time = time.time()
+            if current_time - self.last_reset_time >= self.reset_interval:
+                self.prev_frame = None  # prev_frame을 None으로 설정하여 다음 줄에서 재초기화되도록 함
+                self.last_reset_time = current_time
+                log.info("Reference frame has been reset after 30 minutes")
+            
             if self.prev_frame is None:
                 self.prev_frame = next_frame
                 return False  # 첫 프레임이므로 움직임이 없음
-
+                
             # Calculate optical flow between the previous and current frame
             flow = cv2.calcOpticalFlowFarneback(self.prev_frame, next_frame, None, 0.5, 3, 9, 3, 5, 1.2, 0)
-
+            
             # Calculate the magnitude and angle of the flow
             magnitude, _ = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-
+            
             # Check if the magnitude of the motion is above the threshold
             if np.mean(magnitude) > self.motion_threshold:
                 self.motion_detected = True
@@ -48,13 +58,10 @@ class PouringDetector:
                     if self.buffer_count >= self.buffer_frames:
                         self.motion_detected = False
                 log.debug(f"Magnitude stats - Min: {np.min(magnitude)}, Max: {np.max(magnitude)}, Mean: {np.mean(magnitude)}")
-
                 print("Undetect!")
                 return False
-
         except Exception as e:
             log.error(f"Unexpected error in detect_pouring: {e}")
             return False
-
         finally:
             self.prev_frame = next_frame  # Update previous frame for the next call
